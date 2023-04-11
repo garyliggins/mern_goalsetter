6b123c91196e6d1470a168a8403e30d565b92140
gary <gary@ligginsdevelopment.com>
2023-04-10 19:06:18 -0700

    server is working

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hello dog")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5001 // this is the port the server will run on
+
+const app = express() //this is initializing express into a variable called app
+
+app.listen(port, () => console.log(`Server started on port ${port}`)) //the express server is listening for the server to start and it will run this consolelog
+
+
