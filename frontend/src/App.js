dcecf51d6d84eb9412e003e228cfa61e789a6a75
gary <gary@ligginsdevelopment.com>
2023-06-04 17:02:59 -0700

    starting frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+    <Router>
+    <div className='container'>
+      <h1>my app</h1>
+    </div>
+    </Router> 
+    </>
+  );
+}
+
+export default App;
